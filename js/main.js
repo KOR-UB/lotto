66560a262ac3aff93f99defd0964ca26137bf682
= <yyb2535@gmail.com>
2020-05-27 17:14:38 +0900

    Release

--- js/main.js
+++ js/main.js
@@ -1,37 +1,67 @@
-// 로또 번호 호출
+// // 로또 번호 호출
 
+// // 로또번호를 담을 6자리의 빈배열
+// var lotto = new Array(6);
+
+// // 중복여부 검사
+// var confirm = true;
+
+
+// var count = 0;
+
+// while (count < 6) {
+//   var number = 0;
+//   number = parseInt(Math.random() * 45) + 1
+
+//   for (var i = 0; i <= count; i++) {
+//     if (lotto[i] === number) confirm = false;
+//   }
+
+//   if (confirm) {
+//     lotto[count] = number;
+//     count++;
+//   }
+
+//   confirm = true;
+// }
+
+// const $btn = document.querySelector('.btn');
+// const $div = document.createElement('div');
+// $div.classList.add('lotto');
+
+// $btn.onclick = (e) => {
+//   const $section = document.querySelector('section')
+//   $div.textContent = lotto.join(' ')
+//   $section.appendChild($div);
+//   e.stopPropagation()
+// }
+
+// 로또 번호 호출
 // 로또번호를 담을 6자리의 빈배열
 var lotto = new Array(6);
-
-// 중복여부 검사
-var confirm = true;
-
-
-var count = 0;
-
-while (count < 6) {
-  var number = 0;
-  number = parseInt(Math.random() * 45) + 1
-
-  for (var i = 0; i <= count; i++) {
-    if (lotto[i] === number) confirm = false;
+const getLotto = function () {
+  var confirm = true;
+  var count = 0;
+  while (count < 6) {
+    var number = 0;
+    number = parseInt(Math.random() * 45) + 1
+    for (var i = 0; i <= count; i++) {
+      if (lotto[i] === number) confirm = false;
+    }
+    if (confirm) {
+      lotto[count] = number;
+      count++;
+    }
+    confirm = true;
   }
-
-  if (confirm) {
-    lotto[count] = number;
-    count++;
-  }
-
-  confirm = true;
-}
-
+};
 const $btn = document.querySelector('.btn');
 const $div = document.createElement('div');
-
-$btn.onclick = () => {
-  const $section = document.querySelector('section')
-  $div.textContent = 'lotto.join()'
+$btn.onclick = e => {
+  const $section = document.querySelector('section');
+  getLotto();
+  $div.textContent = lotto.join(' ');
   $section.appendChild($div);
+  lotto = [];
   e.stopPropagation()
 }
-
